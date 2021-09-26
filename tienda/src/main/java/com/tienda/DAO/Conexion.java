1a757124817ac93889a5d9ec9a314b4a7f99d049
Ahmansavthor Batista <ing.batista@outlook.com>
2021-09-25 20:50:05 -0500

    Actualización de proyecto V1

---
+++ tienda/src/main/java/com/tienda/DAO/Conexion.java
@@ -0,0 +1,43 @@
+package com.tienda.DAO;
+
+import java.sql.*;
+public class Conexion {
+	
+	static String bd = "tienda";
+    static String login = "root";
+    static String password = "Proyectomintic1*";
+    static String url = "jdbc:mysql://localhost/"+bd;
+
+    
+    Connection connection = null;
+    
+    public Conexion() {
+        try{
+           //obtenemos el driver de para mysql
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           //obtenemos la conexión
+           connection = DriverManager.getConnection(url,login,password);
+           if (connection!=null){
+              System.out.println("Conexión a base de datos "+bd+" OK\n");
+           }else {
+               System.out.println("No se pudo conectar a la base de datos");
+           }
+        }
+        catch(SQLException e){
+           System.out.println(e);
+        }catch(ClassNotFoundException e){
+           System.out.println(e);
+        }catch(Exception e){
+           System.out.println(e);
+        }
+     }
+    
+    	public Connection getConnection(){
+        return connection;
+     }
+    	public void desconectar(){
+        connection = null;
+     }
+
+
+}
